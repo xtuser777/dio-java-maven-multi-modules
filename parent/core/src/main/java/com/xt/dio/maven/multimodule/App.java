aaa9217017828e114b40dcdf791dfd0441b861ae
Lucas Oliveira <lucaoxt@gmail.com>
2024-04-16 17:02:28 -0300

    Initial

---
+++ parent/core/src/main/java/com/xt/dio/maven/multimodule/App.java
@@ -0,0 +1,13 @@
+package com.xt.dio.maven.multimodule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
